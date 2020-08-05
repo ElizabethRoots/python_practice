c78faf5bc8eefd52f74768c2b47996919145903f
E DeGroot <ElizabethADeGroot@users.noreply.github.com>
2020-08-05 11:34:21 -0400

    Update Control Flow.py

--- Article-Solutions/control-flow/Control Flow.py
+++ Article-Solutions/control-flow/Control Flow.py
@@ -32,3 +32,45 @@ print(over_budget(100, 20, 30, 10, 40))
 # should print False
 print(over_budget(80, 20, 30, 10, 30))
 # should print True
+
+# ------------------------------------------
+
+# Division By Ten
+# Write your divisible_by_ten function here:
+
+
+def divisible_by_ten(num):
+    if (num % 10 == 0):
+        return True
+    else:
+        return False
+
+
+# Uncomment these function calls to test your divisible_by_ten function:
+print(divisible_by_ten(20))
+# should print True
+print(divisible_by_ten(25))
+# should print False
+print(divisible_by_ten(10))
+
+# ------------------------------------------
+
+# Not Equal
+# Write your not_sum_to_ten function here:
+
+
+def not_sum_to_ten(num1, num2):
+    if ((num1 + num2) != 10):
+        return True
+    else:
+        return False
+
+
+# Uncomment these function calls to test your not_sum_to_ten function:
+print(not_sum_to_ten(9, -1))
+# should print True
+print(not_sum_to_ten(9, 1))
+# should print False
+print(not_sum_to_ten(5, 5))
+# should print False
+print(not_sum_to_ten(10, 0))
