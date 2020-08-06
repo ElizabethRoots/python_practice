206ca58009260b19ed1ddc68841e10c8582b7d62
E DeGroot <ElizabethADeGroot@users.noreply.github.com>
2020-08-06 09:51:07 -0400

    Update salshipping.py

--- Article-Solutions/control-flow/salshipping.py
+++ Article-Solutions/control-flow/salshipping.py
@@ -1,31 +1,39 @@
-def cost_of_ground(weight):
-      if (weight >= 8):
-    shipping = 4
-    ground_total = weight * shipping 
-  else:
-    shipping = 1
-    ground_total = weight * shipping
-  return ground_total 
 
-  # testing the function
-  print(cost_of_ground(8.4) + 20)
+def cost_of_ground(weight):
+    if (weight >= 8):
+        shipping = 4
+        ground_total = weight * shipping
+    else:
+        shipping = 1
+        ground_total = weight * shipping
+    return ground_total
+
+    # testing the function
+    print(cost_of_ground(8.4) + 20)
+
 
 def drone_shipping(weight):
-  if (weight <= 3):
-    shipping = 4.50
-    drone_total = weight * shipping
-  else:
-    hipping = 8
-    drone_total = weight * shipping 
-  return drone_total
+    if (weight <= 3):
+        shipping = 4.50
+        drone_total = weight * shipping
+    else:
+        hipping = 8
+        drone_total = weight * shipping
+    return drone_total
 
-  # testing the function
-  print(drone_shipping(1.5)) 
+    # testing the function
+    print(drone_shipping(1.5))
 
-def user_statement(weight): 
-  if (cost_of_ground(weight) < drone_shipping(weight)):
-    print('The cheapest shipping is ' + cost_of_ground(weight) + '.')
-  elif (cost_of_ground(weight) > drone_shipping(weight)):
-    print('The cheapest shipping is ' + drone_shipping(weight) + '.')
-  else:
-    print('Shipping ground and drone is the same cost.') 
+
+def user_statement(weight):
+    ground = ground_total(weight)
+    drone = drone_shipping(weight)
+    premium = premium_shipping
+
+    if drone < ground and drone < premium:
+        print('The cheapest shipping is ' + str
+              (drone) + '.')
+    elif ground < drone and ground < premium:
+        print('The cheapest shipping is ' + str(ground) + '.')
+    else:
+        print('You should ship with ' + str(premium))
