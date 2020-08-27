d14d0901f0e308979c2ef5937d05ba9a7e274608
E DeGroot <ElizabethADeGroot@users.noreply.github.com>
2020-08-26 21:39:14 -0400

    Update coin_flip.py

--- coin_flip/coin_flip.py
+++ coin_flip/coin_flip.py
@@ -1,13 +1,13 @@
 import random
 
 num = random.randint(1, 10)
+heads = 'heads'
+tails = 'tails'
+money = 100
+total = 0
 
 
 def heads_tails():
-    heads = 'heads'
-    tails = 'tails'
-    money = 100
-    total = 0
     coin_guess = input("Choose a side: ")
     bet = int(input("How much to bet? "))
     print(coin_guess)
@@ -20,13 +20,13 @@ def heads_tails():
         total = (money + bet)
         print("You guessed heads and the coin was " + heads + ". You win!")
         print(total)
-    elif (coin_guess == heads) & (coin_flip == 2):
-        total = (money - bet)
-        print("You guessed wrong.")
-        print(total)
     elif (coin_flip == 2) & (coin_guess == tails):
         total = (money + bet)
         print("You guessed tails and the coin was " + tails + ". You win!")
+        print(total)
+    elif (coin_guess == heads) & (coin_flip == 2) | (coin_guess == tails) & (coin_flip == 1):
+        total = (money - bet)
+        print("You guessed wrong.")
         print(total)
 
 
