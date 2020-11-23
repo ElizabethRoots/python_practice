85ba123fb67c481296f8f92608d4162d1ac7d236
E DeGroot <ElizabethADeGroot@users.noreply.github.com>
2020-11-22 21:16:36 -0500

    updating projects

---
+++ medical_insurance_control_flow.py
@@ -0,0 +1,45 @@
+'''
+In this project, you will examine how factors such as age, sex, BMI, number of children, and smoking status contribute to medical insurance costs.
+You will apply your knowledge of Python control flow to write code that gives people advice on how to lower their medical insurance costs.
+'''
+# Add your code here
+
+
+def analyze_smoker(smoker_status):
+    if smoker_status == 1:
+        print("To lower your cost, you should consider quitting smoking.")
+    else:
+        print("Smoking is not an issue for you.")
+    print(smoker_status)
+
+
+def analyze_bmi(bmi_value):
+    if bmi_value > 30:
+        print("Your BMI is in the obese range. To lower your cost, you should significantly lower your BMI.")
+    elif bmi_value >= 25 and bmi_value <= 30:
+        print("Your BMI is in the overweight range. To lower your cost, you should lower your BMI.")
+    elif bmi_value >= 18.5 and bmi_value < 25:
+        print("Your BMI is in a healthy range.")
+    else:
+        print("Your BMI is in the underweight range. Increasing your BMI will not help lower your cost, but it will help improve your health.")
+    print(bmi_value)
+
+# Function to estimate insurance cost:
+
+
+def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
+    estimated_cost = 250*age - 128*sex + 370*bmi + \
+        425*num_of_children + 24000*smoker - 12500
+    print(name + "'s Estimated Insurance Cost: " +
+          str(estimated_cost) + " dollars.")
+    analyze_smoker(smoker)
+    analyze_bmi(bmi)
+    return estimated_cost
+
+
+# Estimate Keanu's insurance cost
+keanu_insurance_cost = estimate_insurance_cost(
+    name='Keanu', age=29, sex=1, bmi=26.2, num_of_children=3, smoker=1)
+
+my_insurance_cost = estimate_insurance_cost(
+    name='me', age=34, sex=0, bmi=36, num_of_children=2, smoker=0)
