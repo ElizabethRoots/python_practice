f94d11b86c17757faacfa2cd7b5efea22c21224f
E DeGroot <ElizabethADeGroot@users.noreply.github.com>
2020-12-08 23:58:12 -0500

    part two of day 1

--- advent_of_code_2020/day1.py
+++ advent_of_code_2020/day1.py
@@ -1,4 +1,5 @@
 # This mini project is part of 2020 Advent of Code https://adventofcode.com/2020
+# Day 1 (both solutions)
 
 import pandas as pd
 import csv
@@ -6,23 +7,19 @@ import csv
 
 df = pd.read_csv(r'saving_christmas_expense.csv', sep=',')
 print(df)
-# for row in df.head().iterrows():
-#    print(row)
 
 a_expense = df.values.tolist()
 b_expense = df.values.tolist()
+c_expense = df.values.tolist()
 print(a_expense)
-'''
-total = expense[0] + expense[1]
-print("totals: ", sum())
-'''
 
 total = []
+total2 = []
 mul_expense = []
 sum_expense = 2020
 new_num1 = []
 new_num2 = []
-
+new_num3 = []
 # For each element in the first list
 for i in range(len(a_expense)):
 
@@ -45,3 +42,16 @@ for i in range(len(a_expense)):
 
             # Print the multiplied result.
             print("The two numbers multiplied equals: ", mul_expense)
+
+        for k in range(len(c_expense)):
+            num3 = c_expense[k]
+            total2 = num1 + num2 + num3
+
+            # If the sum between the thee numbers equals the sum_expense goal
+            if sum(total2) == sum_expense:
+                print("The three numbers added together are: ",
+                      sum(total2), " are ", num1, num2, num3)
+
+                mul_expense2 = [num2 * num1 * num3 for num2,
+                                num1, num3 in zip(a_expense[i], b_expense[j], c_expense[k])]
+                print("The three numbers multiplied together equals: ", mul_expense2)
